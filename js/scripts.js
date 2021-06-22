89f8063505d8547d9f6674069305be22d6e0dc8b
LuisCarazo <luis.carazo@ahinko.com>
2021-06-22 14:38:35 -0600

    se agregan estilos en el menu responsivo movil

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+// funcion para agregar color al menu al dar click en el boton menu
 $('#main-menu-button').on('click', function (e){
     $('#main-nav').toggleClass('bg-primary');
     e.preventDefault();
